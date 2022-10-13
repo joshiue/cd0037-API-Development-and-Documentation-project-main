cf5af18083c9f98d87cf0d5e75d775a00ab1af03
joshiue <jaitonit@yahoo.co.uk>
2022-10-13 20:13:06 +0100

    update

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -45,6 +45,52 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    ### Tests for /categories 
+    def test_get_all_categories(self):
+        res = self.client().get("/categories")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["categories"])
+
+    def test_error_get_all_categories(self):
+        res = self.client().get("/categorie")
+        data = json.loads(res.data)
+        
+        ### print("\n Categories error: ", data)
+        
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "resource absent")
+
+    ### Tests for /questions?page 
+
+    def test_get_paginated_questions(self):
+        res = self.client().get("/questions")
+        data = json.loads(res.data)
+
+        ## confirm the presence every necessary key and values
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data['totalQuestions'])
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['categories'])
+        self.assertTrue(data['currentCategory'])
+
+        ## confirm total number of questions are correct
+        self.assertEqual(len(data['questions']), data['totalQuestions'])
+
+    ### request beyond the valid page number
+
+    def test_404_sent_requesting_beyond_valid_page(self):
+        res = self.client().get("/questions?page=10000")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
 
 
 # Make the tests conveniently executable
